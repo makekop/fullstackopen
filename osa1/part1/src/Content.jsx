634d1f9eb526eaaeea73184ac681311be4a94f02
makekop <72009584+makekop@users.noreply.github.com>
2025-11-24 18:46:53 +0200

    Initial import: osa0, osa1 (with part1), osa2 (with part2)

---
+++ osa1/part1/src/Content.jsx
@@ -0,0 +1,9 @@
+const Content = (props) => {
+    return (
+        <h2>
+            {props.part}: {props.excercises}
+        </h2>
+    );
+};
+
+export default Content;
---
+++ osa1/part1/src/Total.jsx
@@ -0,0 +1,5 @@
+const Total = (props) => {
+    return <h2>Number of excercises in this course = {props.total}</h2>;
+};
+
+export default Total;
