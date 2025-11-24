5e8b056e9d1c03769d571a999a8fd0a2689de116
makekop <72009584+makekop@users.noreply.github.com>
2025-11-24 19:22:55 +0200

    delete wrong folder structure

--- osa1/part1/src/Content.jsx
+++
@@ -1,9 +0,0 @@
-const Content = (props) => {
-    return (
-        <h2>
-            {props.part}: {props.excercises}
-        </h2>
-    );
-};
-
-export default Content;
--- osa1/part1/src/Total.jsx
+++
@@ -1,5 +0,0 @@
-const Total = (props) => {
-    return <h2>Number of excercises in this course = {props.total}</h2>;
-};
-
-export default Total;
